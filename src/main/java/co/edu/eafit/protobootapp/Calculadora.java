d55880978f1e42ce59336fd429d621e55f4bdfbd
Alejandra <acarden6@eafit.edu.co>
2022-04-08 20:48:02 -0500

    test

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -1,11 +1,11 @@
 package co.edu.eafit.protobootapp;
-
+// Operaciones calculadora
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
-    
+
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
